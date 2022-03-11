4496d3ae34573d0a8e06f53caaccaaab5a23b055
BenAwuor2 <ben.oketch2@gmail.com>
2022-03-11 06:40:49 -0800

    Digits

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
  * main - main block
  *
+ * Definition: Get a random number and print it
+ * and if positive,negative or zero
  * Return:0
  */
 int main(void)
@@ -14,9 +17,9 @@ int main(void)
         n = rand() - RAND_MAX / 2;
         if (n>0)
 		printf("%d is positive\n",n);
-	if (n<0)
+	else if (n<0)
 		printf("%d is negative\n",n);
-        if (n==0)
+	else
 		printf("%d is zero\n",n);
         return (0);
 }	
