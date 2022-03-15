40184319091e4a9881f96bf1df44f44cd12a598b
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-15 21:45:41 +0300

    alpha

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+ * print-alphabets - Entry point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
