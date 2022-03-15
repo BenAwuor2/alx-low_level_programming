cc9855437f837cf08398d3fb0f3a8f234711512a
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-15 18:14:18 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - printing putchar
+ *Description: print without using printf
+ *Return:0
+ */
+ 
+int main (void){
+    if (printf("_putchar\n")){
+	
+	}
+	
+    return (0);
+} 
+
