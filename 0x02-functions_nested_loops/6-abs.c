93f3982eff18db56a0076cafeb8bc46fa09c95c9
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-16 14:32:29 +0300

    New

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,28 @@
 #include <stdio.h>
 
 /**
- *_abs - value absolute
+ * _abs - value absolute
  *
  *@n: The int to print
- * Return:0
+ * Return: Always 0.
  */
-int _abs(int i)
+
+int _abs(int n)
 {
-	if (i < 0)
-		i = i * -1;
-	return (i);
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
 
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,29 @@
 #include "main.h"
 
 /**
- *print_last_digit - last digit
+ *  print_last_digit - last digit
  *
- *@n: The in to print
- *Return: Always 0
+ *@n: The int to print
+ * Return: Always 0.
  */
-int print_lasgt_digit(int n)
+
+int print_last_digit(int n)
+
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+int last_digit;
+
+if (n < 0)
+{
+last_digit = (-1 * (n % 10));
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
+else
+{
+last_digit = (n % 10);
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
 }
