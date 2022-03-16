8ae8bc836b9b2da81bfdbbc9eb72850a471b90a6
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-16 11:52:25 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *main - Entry point
  *
@@ -7,13 +8,13 @@
 
 int main(void)
 {
-_putchar( '_' );
-_putchar( 'p' );
-_putchar( 'u' );
-_putchar( 't' );
-_putchar( 'c' );
-_putchar( 'h' );
-_putchar( 'a' );
-_putchar( 'r' );
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
 return(0);
 }
