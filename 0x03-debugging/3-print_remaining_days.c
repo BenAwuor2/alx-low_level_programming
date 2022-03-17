f9c5ab20f13c9c90638563d95efabebbdc464091
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-17 14:32:00 +0300

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,10 +23,8 @@ day++;
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-else
+else if (month == 2 && day == 60)
 {
-if (month == 2 && day == 60)
-}
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
@@ -34,5 +32,5 @@ else
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
-}
+return (0);
 }
