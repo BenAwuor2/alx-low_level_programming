36bfd1704e101c0d5808078b0a8b5caefe0499ac
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-17 21:10:56 +0300

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,11 +10,11 @@
 */
 
 
-void print_remaining_days(int monthj, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 {
-if (month > 2 && day >= 60)
+if (month >= 3 && day >= 60)
 {
 day++;
 }
@@ -23,10 +23,10 @@ day++;
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-else 
+else
 {
 if (month == 2 && day == 60)
-{
+}
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
@@ -34,6 +34,5 @@ else
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
-return (0);
 }
 }
