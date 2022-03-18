32517148e5fa385bbd734b0fa70b6beb88519419
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-18 10:29:04 +0300

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isupper -evalue if letter is uppercase
  * @c: print int
- * Return: Always 0
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
 
 itn _isupper(int c)
@@ -13,8 +13,5 @@ return (1);
 }
 
 else
-{
 return (0);
-}
-
 }
