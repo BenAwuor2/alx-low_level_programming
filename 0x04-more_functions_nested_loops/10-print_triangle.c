3ed661af343b32d6291dbeb124bf5cccf66a5b10
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-20 23:03:35 +0300

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,9 +6,10 @@
  */
 
 void print_triangle(int size)
+
 {
 
-	
+
 int i;
 int z;
 int d;
@@ -19,7 +20,7 @@ if (size > 0)
 d = size - 1;
 for (i = 0; i < size ; i++)
 {
-for (z = d; z > 0; z--)
+for (z = d; z > 0 ; z--)
 {
 _putchar (' ');
 }
@@ -33,9 +34,11 @@ d--;
 
 _putchar ('\n');
 
+
 }
 
 }
+
 else
 {
 _putchar ('\n');
