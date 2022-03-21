3c84ec7d683a30bea061c2ce0e3536c055d9fd2e
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-21 17:41:51 +0300

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_h_
+#define _main_h_
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#endif
