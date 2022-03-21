d836c310022f5ff2f6f6451bfbe08d5deec436c2
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-21 17:48:34 +0300

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,6 @@
 /**
  * reset_to98[D[D_98 - function that takes a pointer to an int as parameter an[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C
  * and updates the value it points to to 98.
- *
  * @n: nomber.
  *
  */
