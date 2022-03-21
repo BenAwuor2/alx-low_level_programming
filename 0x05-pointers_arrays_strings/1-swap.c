513518fb863d5e487d362abef49cc5b3f2538535
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-21 18:11:06 +0300

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ * 
+ *@n: number 1.
+ *
+ *@b: number 2.
+ *
+ * return[D[D[D[D[R[C[C[C[C[C: 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c=[D =[3~[3~ *a;
+*a = *b;
+*b = c;
+}
