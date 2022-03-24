bddfc32255816b24c433d201c3b749f34461a8cb
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-24 23:04:14 +0300

    array

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,21 @@
   
 char *rot13(char *s) 
 { 
-         int i, j; 
+int i, j; 
   
-         char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
-         char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"; 
-  
-         for (i = 0; s[i] != '\0'; i++) 
-         { 
-                 for (j = 0; a[j] != '\0'; j++) 
-                 { 
-                         if (s[i] == a[j]) 
-                         { 
-                                 s[i] = b[j]; 
-                                 break; 
-                         } 
-                 } 
-         } 
-         return (s); 
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"; 
+ 
+for (i = 0; s[i] != '\0'; i++) 
+{ 
+for (j = 0; a[j] != '\0'; j++) 
+{ 
+if (s[i] == a[j]) 
+{ 
+s[i] = b[j]; 
+break; 
+} 
+} 
+} 
+return (s); 
 }
