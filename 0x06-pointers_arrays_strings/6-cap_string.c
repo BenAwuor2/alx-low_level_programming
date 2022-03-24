49965883eae7d342708e3746cf0bea4bea7393be
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-03-24 21:55:36 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,58 +1,28 @@
 #include "main.h
 /**
- * separators - checks and ensure that all string is capitalized
- * @c: character to be checked
- * Return: if separator return 1. Otherwise return 0;
- */
-int separator(char c)
-{
-switch (c)
-{
-case ' ':
-case '\t':
-case '\n':
-case ',':
-case ';':
-case '.':
-case '!':
-case '?':
-case '"':
-case '(':
-case ')':
-case '{':
-case '}':
-return (1);
-
-default:
-return (0);
-}
-
-
-}
-/**
  * cap_string - capitalizes all words of a string
- * @s: string to uppercase
- * Return: returns the modified string
+ * @s: string to modify
+ * Return: returns the resulting string
  */
+
 char *cap_string(char *s)
 {
-int count, upper;
-
-upper = -32;
-
-count = 0;
-
-while (s[count] != '\0')
+int i, j;
+char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+for (i = 0; s[i] != '\0'; i++)
 {
-if (s[count] >= 'a' && s[count] <= 'z')
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+for (j = 0; j < 13; j++)
 {
-
-
-if (s[count] == *s || separator(s[count - 1]))
-
-s[count] += upper;
+if (s[i] == spe[j])
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
 }
-count++;
+}
+}
 }
 return (s);
 }
