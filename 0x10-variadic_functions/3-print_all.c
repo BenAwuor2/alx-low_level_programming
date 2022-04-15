6a92d14bfee419cfbe6eb8e2e96e912531468e34
BenAwuor2 <ben.oketch2@gmail.com>
2022-04-15 04:22:49 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,49 @@
 #include "variadic_functions.h"
-/**
- * print_c - Prints char
- * @list: arguments
- * Return: void
- */
-void print_c(va_list list)
-{
-	printf("%c", (char)va_arg(list, int));
-}
-/**
- * print_d - Prints digit
- * @list: arguments
- * Return: void
- */
-void print_d(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-/**
- * print_f - Prints float
- * @list: arguments
- * Return: void
- */
-void print_f(va_list list)
-{
-	printf("%f", (float)va_arg(list, double));
-}
-/**
- * print_str - Prints str
- * @list: arguments
- * Return: void
- */
-void print_s(va_list list)
-{
-	char *str = va_arg(list, char *);
-
-	if (str)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
-}
+#include <stdio.h>
 
 /**
- * print_all - Prints anything
- * @format: format to print
- * Return: void
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	char *separator = "";
-	int i = 0, j;
+	int i = 0;
+	char *str, *sep = "";
 
-	filter filt[] = {
-	    {'c', print_c},
-	    {'i', print_d},
-	    {'f', print_f},
-	    {'s', print_s}
-	};
+	va_list list;
+
 	va_start(list, format);
-	while (format && format[i])
+
+	if (format)
 	{
-		j = 0;
-		while (j < 4)
+		while (format[i])
 		{
-			if (format[i] == filt[j].fmt)
+			switch (format[i])
 			{
-				printf("%s", separator);
-				filt[j].f(list);
-				separator = ", ";
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
-			j++;
+			sep = ", ";
+			i++;
 		}
-		i++;
 	}
+
 	printf("\n");
 	va_end(list);
 }
-/* hello Lau */
