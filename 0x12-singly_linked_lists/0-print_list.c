ac0c4f06d14e1a447a1c26f42a4f1b2b5ab338f6
BenAwuor2 <ben.oketch2@gmail.com>
2022-04-22 00:33:58 -0700

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+        size_t i;
 
-	for (i = 0; h; i++)
-	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-	}
-	return (i);
+        for (i = 0; h; i++)
+        {
+                printf("[%d] %s\n", h->len, h->str);
+                h = h->next;
+        }
+        return (i);
 }
