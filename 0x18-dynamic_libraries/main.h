6506b476f1d88cda86ab6b060036d6e065e2a89f
BenAwuor2 <ben.oketch2@gmail.com>
2022-06-14 02:36:53 -0700

    advance

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,6 +3,11 @@
 
 #include <stdlib.h>
 
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
